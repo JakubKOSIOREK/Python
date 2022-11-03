54cade091cf19805ae62231eef2356b638aa1a20
Jakub <92948120+JakubKOSIOREK@users.noreply.github.com>
2022-11-03 16:20:39 +0100

    Commit no.3 - update files

--- LINUX/ldap-searchlight/scripts/ldap-users-searchlight.py
+++ LINUX/ldap-searchlight/scripts/ldap-users-searchlight.py
@@ -1,42 +1,103 @@
 #! /usr/bin/python3
 
 import configparser
-from distutils.log import error
 import os
 import getpass
 import sys
+import ssl
+from ldap3 import Server, Connection, Tls, ALL_ATTRIBUTES
+import json
 
 # screen cleaner
 os.system('cls')
 
 # load variables from config files
 config = configparser.RawConfigParser()
-#config.read('/tmp/ldap-searchlight/config/searchlight.conf')
-config.read('C:\GitHub\Python\LINUX\ldap-searchlight\config\searchlight.conf')
+config.read('/tmp/ldap-searchlight/config/searchlight.conf')
+ldap_base_dn = config['GLOBAL']['ldap_base_dn']
+user1_name = config['USERS']['user1_name']
+user2_name = config['USERS']['user2_name']
+ldap_users_dn = config['USERS']['ldap_users_dn']
 
-# VARIABLES
-user1_name = config['USERS']['user1-name']
-ldap_user1 = config['USERS']['ldap-user1']
-
-user2_name = config['USERS']['user2-name']
-ldap_user2 = config['USERS']['ldap-user2']
-
-ldap_base_dn = config['GLOBAL']['ldap-base-dn']
-
-# login & password input
-input_name = input("Podaj imie i nazwisko:\n")
-if input_name == user1_name:
-    name = user1_name
-    password = getpass.getpass()
-elif input_name == user2_name:
-    name = user2_name
-    password = getpass.getpass()
+# user's choice
+print(
+    "Sing in as:\n" +
+    "[ 1 ] for " + user1_name + "\n" +
+    "[ 2 ] for " + user2_name + "\n"
+)
+input_value = input("Select the user's number:")
+if input_value == "1":
+    user = config['USERS']['ldap_user1']
+elif input_value == "2":
+    user = config['USERS']['ldap_user1']
 else:
-    print("Permission Danied")
+    print("PERMISSION DANIED\n")
     sys.exit(1)
 
+# getting password
+password = getpass.getpass()
 
+# LDAP
+# ldap variables
 LDAP_HOST = config['GLOBAL']['ldap-host']
+LDAP_BASE_DN = ldap_users +","+ ldap_base_dn
+LDAP_USER = user +","+ ldap_users +","+ ldap_base_dn
+LDAP_PASSWORD = password
+LDAP_OBJECT_FILTER = '(objectclass=user)'
+tls_configuration = Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1)
+# users attributes list
+user_attr_list=[ \
+    'cn', \
+    'givenName', \
+    'instanceType', \
+    'whenCreated', \
+    'displayName', \
+    'uSNCreated', \
+    'name', \
+    'objectGUID', \
+    'badPwdCount', \
+    'codePage', \
+    'countryCode', \
+    'badPasswordTime', \
+    'lastLogoff', \
+    'lastLogon',\
+    'primaryGroupID', \
+    'objectSid', \
+    'accountExpires', \
+    'logonCount', \
+    'sAMAccountName', \
+    'sAMAccountType', \
+    'userPrincipalName', \
+    'objectCategory', \
+    'pwdLastSet', \
+    'userAccountControl', \
+    'lastLogonTimestamp', \
+    'whenChanged', \
+    'uSNChanged', \
+    'memberOf', \
+    'distinguishedName' ]
 
+# connecting to the domain controller
+def ldap_server():
+    return Server(LDAP_HOST,
+                  use_ssl=True,
+                  tls=tls_configuration,
+                  get_info=ALL_ATTRIBUTES)
 
-print(LDAP_HOST)
+def ldap_connection():
+    server = ldap_server(), 
+    return Connection(server, user=LDAP_USER,
+                      password=LDAP_PASSWORD,
+                      auto_bind=True)
+
+conn = ldap_connection()
+conn.search(LDAP_BASE_DN, LDAP_OBJECT_FILTER, attributes=user_attr_list)
+
+# output to json file
+json_file_path = '/tmp/ldap-searchlight/data/ldap-users.json'
+data = json.loads(conn.response_to_json())
+with open(json_file_path, 'w') as jsonfile:
+    json.dump(data, jsonfile)
+
+# end
+print("SUCCES, data saved in: " + json_file_path)
